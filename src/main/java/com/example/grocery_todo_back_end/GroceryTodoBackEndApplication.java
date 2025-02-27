8984593cb686b7a18e7f19c4edfe1e2862fe8f75
Reshzera <reoshiro13@gmail.com>
2025-02-27 18:10:38 -0400

    feat: first java commit

---
+++ src/main/java/com/example/grocery_todo_back_end/GroceryTodoBackEndApplication.java
@@ -0,0 +1,12 @@
+package com.example.grocery_todo_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryTodoBackEndApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryTodoBackEndApplication.class, args);
+	}
+
+}
