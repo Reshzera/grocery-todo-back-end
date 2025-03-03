6c68710de5c453601e351743b0f1f6b8534a9ce0
Reshzera <reoshiro13@gmail.com>
2025-03-03 19:08:58 -0400

    feat: start spring boot first setup

---
+++ src/main/java/com/example/grocery_todo_back_end/GroceryTodoService.java
@@ -0,0 +1,10 @@
+package com.example.grocery_todo_back_end;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GroceryTodoService {
+
+    public void createItem(){
+        System.out.println("Item was created");
+    }
+}
